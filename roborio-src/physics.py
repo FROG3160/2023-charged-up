067752fe0ac6fa33acbc17b6c87d4625ea16a905
Brian Jarrett <celttechie@gmail.com>
2023-01-19 19:24:13 -0600

    Added basic physics.py for sim

---
+++ roborio-src/physics.py
@@ -0,0 +1,47 @@
+#
+# See the documentation for more details on how this works
+#
+# Documentation can be found at https://robotpy.readthedocs.io/projects/pyfrc/en/latest/physics.html
+#
+# The idea here is you provide a simulation object that overrides specific
+# pieces of WPILib, and modifies motors/sensors accordingly depending on the
+# state of the simulation. An example of this would be measuring a motor
+# moving for a set period of time, and then changing a limit switch to turn
+# on after that period of time. This can help you do more complex simulations
+# of your robot code without too much extra effort.
+#
+# Examples can be found at https://github.com/robotpy/examples
+
+import wpilib.simulation
+
+from pyfrc.physics.core import PhysicsInterface
+from pyfrc.physics.drivetrains import four_motor_swerve_drivetrain, linear_deadzone
+
+
+# import typing
+
+# if typing.TYPE_CHECKING:
+from robot import FROGbot
+
+
+class PhysicsEngine:
+    """
+    Simulates a 4-wheel robot using Tank Drive joystick control
+    """
+
+    def __init__(self, physics_controller: PhysicsInterface, robot: "FROGbot"):
+        """
+        :param physics_controller: `pyfrc.physics.core.Physics` object
+                                   to communicate simulation effects to
+        :param robot: your robot objet
+        """
+
+        self.physics_controller = physics_controller
+        self.robot = robot
+
+
+    def update_sim(self, now, tm_diff):
+        chassis_speeds = self.robot.swerveChassis.speeds
+        pose = self.physics_controller.drive(chassis_speeds, tm_diff)
+
+        self.robot.gyro.setAngleAdjustment(-pose.rotation().degrees())
