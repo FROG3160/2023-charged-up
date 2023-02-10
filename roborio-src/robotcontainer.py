6ad198fa8c9a29e602824fd01ab40794a78b3668
Brian Jarrett <celttechie@gmail.com>
2023-02-09 19:52:28 -0600

    Added xbox controller, arm, grabber, trajectory and auto drive.
    Co-authored-by: Myla-Kelly <Myla-Kelly@users.noreply.github.com>
    Co-authored-by: Joseph Wang <Joseph3160@users.noreply.github.com>

--- roborio-src/robotcontainer.py
+++ roborio-src/robotcontainer.py
@@ -1,26 +1,75 @@
+from wpimath.trajectory import TrajectoryGenerator, TrajectoryConfig, TrapezoidProfileRadians
 from subsystems.drivetrain import SwerveChassis
 from subsystems.controllers import FROGStick, LOGITECH_EXTREME_AXIS_CONFIG
 from subsystems.vision import FROGPhotonVision
-from commands2.button import JoystickButton
+from subsystems.arm import Arm
+from subsystems.grabber import FROGGrabber
+from commands2.button import JoystickButton, CommandXboxController
+from commands2 import RunCommand
+from wpimath.units import feetToMeters
+from wpimath.geometry import Transform2d
 
-from commands.swerve_commands import cmdFieldOrientedDrive, cmdFieldOrientedThrottledDrive, cmdZeroGyro
+from commands.swerve_commands import cmdFieldOrientedDrive, cmdFieldOrientedThrottledDrive, cmdZeroGyro, cmdDriveTrajectory
+
+MAX_TRAJECTORY_SPEED = feetToMeters(5)
+MAX_TRAJECTORY_ACCEL = feetToMeters(5)
+
+trajectoryConfig = TrajectoryConfig(MAX_TRAJECTORY_SPEED, MAX_TRAJECTORY_ACCEL)
 
 
 class RobotContainer:
     def __init__(self):
 
         self.driverController = FROGStick(port = 0, **LOGITECH_EXTREME_AXIS_CONFIG)
+        self.operatorController = CommandXboxController(2)
 
         # Robot Subsystems
         self.swerveChassis = SwerveChassis()
         self.swerveChassis.setDefaultCommand(
             cmdFieldOrientedThrottledDrive(self.driverController, self.swerveChassis)
         )
+        self.arm = Arm(41, 42)
+        self.arm.setDefaultCommand(
+            RunCommand(
+                lambda: self.arm.manual(
+                    self.operatorController.getRightY(),
+                    self.operatorController.getLeftY()
+                    ),
+                    [self.arm]
+            )
+        )
+        
+        self.grabber = FROGGrabber(51, 52, 1)
+        self.grabber.setDefaultCommand(
+            RunCommand(
+                lambda: self.grabber.wheelsOn(
+                    self.operatorController.getLeftTriggerAxis()
+                    ),
+                    [self.arm]
+            )
+        )
+
 
         self.btnZeroGyro = JoystickButton(self.driverController, 3)
+        self.btnDriveAuto = JoystickButton(self.driverController, 2)
+
+        trajectoryConfig.setKinematics(self.kinematics)
+        self.trajectory = TrajectoryGenerator.generateTrajectory(
+			self.swerveChassis.estimatorPose, # Starting position
+			[], # Pass through these points
+			self.swerveChassis.estimatorPose + Transform2d(feetToMeters(6), feetToMeters(3)),
+             # Ending position
+			trajectoryConfig
+        )
+
         self.configureButtonBindings()
 
     def configureButtonBindings(self):
         self.btnZeroGyro.onTrue(
             cmdZeroGyro(self.swerveChassis)
+        )
+        self.btnDriveAuto.whileTrue(
+            cmdDriveTrajectory(
+                self.swerveChassis, self.trajectory
+            )
         )
--- roborio-src/subsystems/arm.py
+++ roborio-src/subsystems/arm.py
@@ -1,14 +1,11 @@
-
 from commands2 import SubsystemBase
 from ctre import WPI_TalonFX
 
 
-class Arm(SubsystemBase):
-    
-
+class SubArm(SubsystemBase):
     def __init__(
         self,
-        motorID = int,
+        motorID=int,
     ):
         super().__init__()
         self.motor = WPI_TalonFX(motorID)
@@ -16,13 +13,22 @@ class Arm(SubsystemBase):
     def getEncoderPosition(self) -> float:
         return self.motor.getSelectedSensorPosition()
 
-
     def run(self, speed: float):
-    # TODO: Determine the actual position the arm is moving in based on 
-    # running the motor forward and backward.
+        # TODO: Determine the actual position the arm is moving in based on
+        # running the motor forward and backward.
         self.motor.set(speed)
 
     def stop(self):
         self.motor.set(0)
 
-    
+
+class Arm(SubsystemBase):
+    def __init__(self, boomMotorID, stickMotorID) -> None:
+        super().__init__()
+
+        self.boom = SubArm(boomMotorID)
+        self.stick = SubArm(stickMotorID)
+
+    def manual(self, boomSpeed, stickSpeed):
+        self.boom.run(boomSpeed)
+        self.stick.run(stickSpeed)
