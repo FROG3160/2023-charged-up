4ec02bb0598687e2203a44694bc3f0c0ff8eb918
Brian Jarrett <celttechie@gmail.com>
2023-03-02 15:32:30 -0600

    PID changes, added min speed

--- roborio-src/config.py
+++ roborio-src/config.py
@@ -17,10 +17,12 @@ WHEELBASE = 20 / 12
 
 # SwerveDriveSpecialties modules have the following max speeds (in ft/sec):
 # L1 - 13.5, L2 - 16.3, L3 - 18
-MAX_FEET_PER_SEC = 10
-MAX_METERS_PER_SEC = MAX_FEET_PER_SEC * 0.3038
+MAX_FEET_PER_SEC = 8
+MIN_FEET_PER_SEC = 0.5
+MAX_METERS_PER_SEC = feetToMeters(MAX_FEET_PER_SEC)
+MIN_METERS_PER_SEC = feetToMeters(MIN_FEET_PER_SEC)
 
-MAX_CHASSIS_REV_SEC = 0.5
+MAX_CHASSIS_REV_SEC = 1
 MAX_CHASSIS_RADIANS_SEC = MAX_CHASSIS_REV_SEC * math.tau
 
 MODULE_DRIVE_GEARING = [(14.0 / 50.0), (28.0 / 16.0), (15.0 / 45.0)]  # Mk4 L3
@@ -70,8 +72,8 @@ holonomicTranslationPIDController = PIDController(1.5, 0, 0)
 holonomicAnglePIDController = ProfiledPIDControllerRadians(
     2.5, 0, 0, TrapezoidProfileRadians.Constraints(math.pi, math.pi)
 )
-ppTranslationPIDController = PIDController(1,0,0)
-ppRotationPIDController = PIDController(1,0,0)
+ppTranslationPIDController = PIDController(4,0,0)
+ppRotationPIDController = PIDController(8,0,0)
 
 #
 # **Swerve Module Drive Motor Config
@@ -82,7 +84,7 @@ cfgDriveMotor.primaryPID = BaseTalonPIDSetConfiguration(FeedbackDevice.Integrate
 cfgDriveMotor.slot0.kP = 0.0  # TODO: Confirm PID
 cfgDriveMotor.slot0.kI = 0.0
 cfgDriveMotor.slot0.kD = 0.0
-cfgDriveMotor.slot0.kF = 0.04664  # 0.058
+cfgDriveMotor.slot0.kF = 0.05 #0.04664  # 0.058
 
 #
 # **Swerve Module Steer Motor Config
