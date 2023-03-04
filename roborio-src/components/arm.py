1524666fa0b1e0f3bc189962c4fc90d932fa979f
Brian Jarrett <celttechie@gmail.com>
2023-03-04 17:14:46 -0600

    moved motor initialization added logging and stop when at position

--- roborio-src/components/arm.py
+++ roborio-src/components/arm.py
@@ -1,13 +1,28 @@
-from ctre import WPI_TalonFX, ControlMode, NeutralMode
+from ctre import WPI_TalonFX, ControlMode, NeutralMode, TalonFXConfiguration
 import config
+import logging
 
 
 class SubArm():
     def __init__(
         self,
-        motorID=int,
+        name: str,
+        motorID: int,
+        motorConfig: TalonFXConfiguration,
+        neutralMode: NeutralMode,
+        logger,
+        motorInverted=False,
+        sensorPhase = False,
     ):
+        self.name = name
         self.motor = WPI_TalonFX(motorID)
+        self.motor.configAllSettings(motorConfig)
+        self.motorConfig = motorConfig
+        self.motor.setNeutralMode(neutralMode)
+        self.motor.setInverted(motorInverted)
+        self.motor.setSensorPhase(sensorPhase)
+        self.commandedPosition = None
+        self.logger = logger
 
     def getEncoderPosition(self) -> float:
         return self.motor.getSelectedSensorPosition()
@@ -18,27 +33,37 @@ class SubArm():
         self.motor.set(speed)
 
     def toPosition(self, position: float):
+        self.commandedPosition = position
         self.motor.set(ControlMode.MotionMagic, position)
 
     def stop(self):
         self.motor.set(0)
 
+    def atPosition(self):
+        self.motor.getActiveTrajectoryPosition()
+    
+    def execute(self):
+        #Check to see if we are running Motion Magic
+
+        if self.commandedPosition is not None:
+            if abs(self.motor.getActiveTrajectoryPosition() - self.commandedPosition) < 200:
+                self.logger.info(f"SubArm: {self.name} - Stopped at {self.motor.getActiveTrajectoryPosition()} with commanded Position: {self.commandedPosition}")
+                self.commandedPosition = None
+                self.stop()
+
 
 class Arm():
-    def __init__(self, boomMotorID, stickMotorID) -> None:
+    def __init__(self) -> None:
 
-        self.boom = SubArm(boomMotorID)
-        self.boom.motor.configAllSettings(config.cfgBoomMotor)
-        self.boom.motor.setNeutralMode(NeutralMode.Brake)
-        self.boom.motor.setInverted(False)
-        self.boom.motor.setSensorPhase(False)
-
-        self.stick = SubArm(stickMotorID)
-        self.stick.motor.configAllSettings(config.cfgStickMotor)
-        self.stick.motor.setNeutralMode(NeutralMode.Brake)
-        self.stick.motor.setInverted(False)
-        self.stick.motor.setSensorPhase(False)
+        self.logger = logging.getLogger("FROGArm")
+        self.boom = SubArm("Boom", config.BOOM_MOTOR_ID, config.cfgBoomMotor, NeutralMode.Brake, logger = self.logger)
+        self.stick = SubArm("Stick", config.STICK_MOTOR_ID, config.cfgStickMotor, NeutralMode.Brake, logger = self.logger)
 
     def manual(self, boomSpeed, stickSpeed):
         self.boom.run(boomSpeed)
         self.stick.run(stickSpeed)
+
+    def execute(self):
+        self.boom.execute()
+        self.stick.execute()
+        pass
