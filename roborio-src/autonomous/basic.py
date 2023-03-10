1dc4317144fe827196738d8f6a417f9707eb5144
Brian Jarrett <celttechie@gmail.com>
2023-03-10 04:59:02 -0600

    Added place cube auto routine

--- roborio-src/autonomous/basic.py
+++ roborio-src/autonomous/basic.py
@@ -1,22 +1,93 @@
 from logging import Logger
 
 from components.drivetrain import SwerveChassis
+from components.arm_control import ArmControl, GrabberControl
 from magicbot import AutonomousStateMachine, state, timed_state
+from wpimath.geometry import Transform2d, Pose2d, Rotation2d
+from wpimath.units import degreesToRadians, inchesToMeters, feetToMeters
+from pathplannerlib import PathPoint
 
 
 class moveToGridRight(AutonomousStateMachine):
-    MODE_NAME = "Move to Grid Right"
+    MODE_NAME = "Place cone"
 
     swerveChassis: SwerveChassis
+    armControl: ArmControl
+    grabberControl: GrabberControl
+    startX = 2.47
+    endX = 1.81
 
     @state(first=True)
-    def loadTrajectory(self, initial_call):
+    def raiseArm(self, initial_call):
         if initial_call:
-            self.swerveChassis.holonomicController.loadPathPlanner(
-                "mid-grid_charging-right"
+            self.logger.info(f'Vision Estimate: {self.swerveChassis.visionPoseEstimator.getEstimatedRobotPose()}')
+            self.swerveChassis.setFieldPosition(Pose2d(self.startX,0,0))
+            self.logger.info(f'Estimator Field Position is: {self.swerveChassis.estimator.getEstimatedPosition()}')
+            startTrajectoryPose = self.swerveChassis.estimator.getEstimatedPosition()
+            endTrajectoryPose = startTrajectoryPose.transformBy(
+                Transform2d(self.endX - self.startX, 0 , 0)
             )
-        self.next_state("runTrajectory")
+            self.logger.info(f'Starting at {startTrajectoryPose}')
+            self.logger.info(f'Ending at: {endTrajectoryPose}')
+            ##TODO figure out how to calculate heading
+            startPoint = PathPoint(startTrajectoryPose.translation(), startTrajectoryPose.rotation())
+            endPoint = PathPoint(endTrajectoryPose.translation(), endTrajectoryPose.rotation())
+            self.swerveChassis.holonomicController.initSimpleTrajectory(
+                startPoint, # Starting position
+                endPoint, # Ending position
+            )
+            self.logger.info(f'Raising arm to upper')
+            self.armControl.next_state('moveToUpper')
+        self.armControl.engage()
+        if self.armControl.last_state == 'atUpper':
+            self.next_state('moveBack')
 
-    @state
-    def runTrajectory(self):
+    @state()
+    def moveBack(self, initial_call):
         self.swerveChassis.autoDrive()
+        if self.swerveChassis.holonomicController.atReference():
+            self.next_state("dropCone")
+
+    @state()
+    def dropCone(self, initial_call):
+        if initial_call:
+            self.logger.info(f'Vision Estimate: {self.swerveChassis.visionPoseEstimator.getEstimatedRobotPose()}')
+            self.swerveChassis.setFieldPosition(Pose2d(self.startX,0,0))
+            self.logger.info(f'Estimator Field Position is: {self.swerveChassis.estimator.getEstimatedPosition()}')
+            startTrajectoryPose = self.swerveChassis.estimator.getEstimatedPosition()
+            endTrajectoryPose = startTrajectoryPose.transformBy(
+                Transform2d(self.startX - self.endX, 0 , 0)
+            )
+            self.logger.info(f'Starting at {startTrajectoryPose}')
+            self.logger.info(f'Ending at: {endTrajectoryPose}')
+            ##TODO figure out how to calculate heading
+            startPoint = PathPoint(startTrajectoryPose.translation(), startTrajectoryPose.rotation())
+            endPoint = PathPoint(endTrajectoryPose.translation(), endTrajectoryPose.rotation())
+            self.swerveChassis.holonomicController.initSimpleTrajectory(
+                startPoint, # Starting position
+                endPoint, # Ending position
+            )
+            self.grabberControl.next_state('dropping')
+        self.grabberControl.engage()
+        if self.grabberControl.last_state == 'looking':
+            self.next_state('moveForward')
+    
+    @state()
+    def moveForward(self, initial_call):
+        self.swerveChassis.autoDrive()
+        if self.swerveChassis.holonomicController.atReference():
+            self.next_state('dropArm')
+        
+    @state()
+    def dropArm(self, initial_call):
+        if initial_call:
+            self.armControl.next_state('moveToHome')
+        self.armControl.engage()
+        if self.armControl.last_state == 'atHome':
+            self.next_state('end')
+
+    @state()
+    def end(self):
+        self.done()
+
+
