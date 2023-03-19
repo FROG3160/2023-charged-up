600270f9ddc96e8807579653caf40660ad2db058
Brian Jarrett <celttechie@gmail.com>
2023-03-19 15:08:10 -0500

    refactored limelight target calculations

--- roborio-src/components/vision.py
+++ roborio-src/components/vision.py
@@ -24,9 +24,6 @@ def arrayToPose3d(poseArray) -> Pose3d:
     )
 
 
-
-
-
 class FROGLimeLightVision:
     fieldLayout: FROGFieldLayout
 
@@ -107,12 +104,38 @@ class FROGLimeLightVision:
             self.ta = self.grabberTa.get()
             self.tx = self.grabberTx.get()
             self.tv = self.grabberTv.get()
-            self.drive_vRotate = -(self.tx / 40)
-            self.drive_vX = -(self.ta * -0.0098 + 1.0293)
+            self.drive_vRotate = self.calculateRotation(self.tx)
+            self.drive_vX = self.calculateX(self.ta)
             self.drive_vY = 0
         else:
             self.tClass = self.ta = self.tx = self.tv = None
             self.drive_vRotate = self.drive_vX = self.drive_vY = 0
+
+    def calculateX(self, targetArea):
+        """Calculate X robot-oriented speed from the size of the target.  Return is inverted
+        since we need the robot to drive backwards toward the target to pick it up.
+
+        Args:
+            targetArea (Float):  The target area determined by limelight.
+
+        Returns:
+            Float: Velocity in the X direction (robot oriented)
+        """
+        return -(targetArea * -0.0098 + 1.0293)
+
+    def calculateRotation(self, targetX):
+        """Calculate the rotational speed from the X value of the target in the camera frame.
+        Return is inverted to make left rotation positive from a negative X value, meaning the
+        target is to the left of center of the camera's view.
+
+        Args:
+            targetX (Float): The X value of the target in the camera frame, 0 is straight ahead,
+            to the left is negative, to the right is positive.
+
+        Returns:
+            Float: Rotational velocity with CCW (left, robot oriented) positive.
+        """
+        return -(targetX / 40)
 
     def getVelocities(self):
         """Get calculated velocities from vision target data
